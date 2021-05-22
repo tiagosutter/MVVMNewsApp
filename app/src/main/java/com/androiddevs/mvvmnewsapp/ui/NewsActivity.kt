f23eb96cc2d6a3526629b2be45ea07826ed5c1ac
tiagosutter <tiagosutter@gmail.com>
2021-05-22 13:30:11 -0300

    Setup navigation between fragments

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsActivity.kt
@@ -0,0 +1,22 @@
+package com.androiddevs.mvvmnewsapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.androiddevs.mvvmnewsapp.R
+import com.androiddevs.mvvmnewsapp.databinding.ActivityNewsBinding
+
+class NewsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityNewsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityNewsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = findNavController(R.id.newsNavHostFragment)
+        binding.bottomNavigationView.setupWithNavController(navHostFragment)
+    }
+}
