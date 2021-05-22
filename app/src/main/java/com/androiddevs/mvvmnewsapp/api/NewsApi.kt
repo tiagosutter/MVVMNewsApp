b697dc9780b5b10091481065d7c88536608f60ab
tiagosutter <tiagosutter@gmail.com>
2021-05-22 16:25:02 -0300

    Room setup

--- app/src/main/java/com/androiddevs/mvvmnewsapp/api/NewsApi.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/api/NewsApi.kt
@@ -1,7 +1,7 @@
 package com.androiddevs.mvvmnewsapp.api
 
 import com.androiddevs.mvvmnewsapp.BuildConfig
-import com.androiddevs.mvvmnewsapp.model.NewsResponse
+import com.androiddevs.mvvmnewsapp.models.NewsResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
--- app/src/main/java/com/androiddevs/mvvmnewsapp/models/Article.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/models/Article.kt
@@ -1,6 +1,14 @@
-package com.androiddevs.mvvmnewsapp.model
+package com.androiddevs.mvvmnewsapp.models
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "articles"
+)
 data class Article(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int? = null,
     val author: Any,
     val content: String,
     val description: String,
